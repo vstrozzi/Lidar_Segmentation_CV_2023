7297a3af82c96c3f63e143735a4a7abf17579e8e
Luca Brodo <brodo.luca001@stud.fh-dortmund.de>
2023-11-08 10:54:19 +0100

    restructuring and starting testing clustering

--- code/test_clustering.py
+++ code/test_clustering.py
@@ -1,9 +1,14 @@
 #from sklearn.cluster import DBSCAN
 #from LidarMapping.calibration import Calibration
+import os
+import cv2
+import numpy as np
+from LidarMapping.calibration import Calibration
+from LidarMapping.depth_map import dense_map
 
-import LidarMapping
+
 if __name__ == "__main__":
-    root = "test_data/"
+    root = "LidarMapping/test_data/"
     image_dir = os.path.join(root, "image_2")
     velodyne_dir = os.path.join(root, "velodyne")
     calib_dir = os.path.join(root, "calib")
@@ -12,6 +17,12 @@ if __name__ == "__main__":
     img = cv2.imread(os.path.join(image_dir, "%06d.png" % cur_id))
     lidar = np.fromfile(os.path.join(velodyne_dir, "%06d.bin" % cur_id), dtype=np.float32).reshape(-1, 4)
     calib = Calibration(os.path.join(calib_dir, "%06d.txt" % cur_id))
+    lidar_rect = calib.lidar2cam(lidar[:,0:3])
+    lidarOnImage, mask = calib.rect2Img(lidar_rect, img.shape[1], img.shape[0])
+    lidarOnImage = np.concatenate((lidarOnImage, lidar_rect[mask,2].reshape(-1,1)), 1)
+    out = dense_map(lidarOnImage.T, img.shape[1], img.shape[0], 6)
+    print(out.shape)
+
 
 #dbscan = DBSCAN(eps = 8, min_samples = 4).fit(x) # fitting the model
 #labels = dbscan.labels_ # getting the labels
