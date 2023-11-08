6e4fefbbc18eafaff25c069ba5aec76e4cfa3f8a
Luca Brodo <brodo.luca001@stud.fh-dortmund.de>
2023-11-08 11:01:13 +0100

    test image and clustering continuted

--- code/test_clustering.py
+++ code/test_clustering.py
@@ -5,7 +5,9 @@ import cv2
 import numpy as np
 from LidarMapping.calibration import Calibration
 from LidarMapping.depth_map import dense_map
+from sklearn.cluster import DBSCAN
 
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     root = "LidarMapping/test_data/"
@@ -21,8 +23,22 @@ if __name__ == "__main__":
     lidarOnImage, mask = calib.rect2Img(lidar_rect, img.shape[1], img.shape[0])
     lidarOnImage = np.concatenate((lidarOnImage, lidar_rect[mask,2].reshape(-1,1)), 1)
     out = dense_map(lidarOnImage.T, img.shape[1], img.shape[0], 6)
-    print(out.shape)
-
+    rows, cols, chs = out.shape
+    indices = np.dstack(np.indices(out.shape[:2]))
+    xycolors = np.concatenate((img, indices), axis=-1) 
+    xycolors = np.reshape(xycolors, [-1,5])
+    print(xycolors.shape)
+    db = DBSCAN(eps=5, min_samples=50, metric = 'euclidean',algorithm ='auto')
+    db.fit(xycolors)
+    labels = db.labels_
+    plt.figure(2)
+    plt.subplot(2, 1, 1)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.subplot(2, 1, 2)
+    plt.imshow(np.reshape(labels, [rows, cols]))
+    plt.axis('off')
+    plt.show()
 
 #dbscan = DBSCAN(eps = 8, min_samples = 4).fit(x) # fitting the model
 #labels = dbscan.labels_ # getting the labels
