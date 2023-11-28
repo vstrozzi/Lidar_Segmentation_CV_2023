a531ca9fb75d8c317b0927e560c53027a03b2450
vstrozzi <147145259+vstrozzi@users.noreply.github.com>
2023-11-28 12:00:58 +0100

    Update modeltolightning.py
    Added image log

--- models/modeltolightning.py
+++ models/modeltolightning.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import pytorch_lightning as pl
 from utils.dataloader.labels import *
+import wandb
 
 class LightningModel(pl.LightningModule):
     def __init__(self, model, loss, eval_metric, optimizer=torch.optim.Adam, lr=1e-4, mode="RGB"):
@@ -49,6 +50,7 @@ class LightningModel(pl.LightningModule):
         else:
             out = x["left_rgb"]
 
+        
         out = self.forward(out)
         loss = self.loss(out, y)
         
@@ -56,6 +58,18 @@ class LightningModel(pl.LightningModule):
         score = self.eval_metric(pred.squeeze(), y.squeeze())
         self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("val_score", score, on_step=False, on_epoch=True, prog_bar=True)
+
+        if batch_idx == 2:
+            NR_IN_BATCH = 0
+            class_labels = {0: "person", 1: "rider", 2: "vehicle", 3:"others"}
+            
+            mask_img = wandb.Image(
+                x["left_rgb"][NR_IN_BATCH],
+                masks={
+                    "predictions": {"mask_data": pred[NR_IN_BATCH], "class_labels": class_labels},
+                    "ground_truth": {"mask_data": y[NR_IN_BATCH], "class_labels": class_labels},
+                })
+            self.log({"img_with_masks": masked_image}
     
     def configure_optimizers(self):
         return self.optimizer(self.parameters(), lr=self.lr)
