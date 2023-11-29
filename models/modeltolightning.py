9f3a479a853b279ab244db54ff98a4b89634986e
vstrozzi <147145259+vstrozzi@users.noreply.github.com>
2023-11-29 09:24:54 +0100

    Change layout a bit

--- models/modeltolightning.py
+++ models/modeltolightning.py
@@ -59,18 +59,20 @@ class LightningModel(pl.LightningModule):
         self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("val_score", score, on_step=False, on_epoch=True, prog_bar=True)
 
-        mask_list = []
-        for i in range(0, 5):
-            class_labels = {0: "person", 1: "rider", 2: "vehicle", 3:"others"}  
-            mask_img = wandb.Image(
-                x["left_rgb"][i],
-                masks={
-                    "predictions": {"mask_data": pred[i].numpy(force=True), "class_labels": class_labels},
-                    "ground_truth": {"mask_data": y[i].squeeze().numpy(force=True), "class_labels": class_labels},
-                })
-            mask_list.append(mask_img)
+        # Only on first batch
+        if batch_idx == 0:
+            mask_list = []
+            for i in range(0, 10):
+                class_labels = {0: "person", 1: "rider", 2: "vehicle", 3:"others"}  
+                mask_img = wandb.Image(
+                    x["left_rgb"][i],
+                    masks={
+                        "predictions": {"mask_data": pred[i].numpy(force=True), "class_labels": class_labels},
+                        "ground_truth": {"mask_data": y[i].squeeze().numpy(force=True), "class_labels": class_labels},
+                    })
+                mask_list.append(mask_img)
 
-        self.logger.experiment.log({"image": mask_list})
+            self.logger.experiment.log({"image": mask_list})
     
     def configure_optimizers(self):
         return self.optimizer(self.parameters(), lr=self.lr)
