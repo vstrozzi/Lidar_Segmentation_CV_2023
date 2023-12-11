3c9f90f230e16c01d9eeb51b11f48b55b941824f
vstrozzi <147145259+vstrozzi@users.noreply.github.com>
2023-12-11 09:20:56 +0100

    commit fix out

--- models/modeltolightning.py
+++ models/modeltolightning.py
@@ -28,7 +28,7 @@ class LightningModel(pl.LightningModule):
         if self.mode == "LIDAR":
             out = x["left_disp"]
         elif self.mode == "LIDAR-RGB":
-            out = torch.cat((x["left_rgb"], x["left_disp"]), 1)
+            out = 0.95*x["left_rgb"] + 0.05*torch.cat((x["left_disp"], x["left_disp"], x["left_disp"]), 1)
         else:
             out = x["left_rgb"]
 
@@ -47,7 +47,6 @@ class LightningModel(pl.LightningModule):
         if self.mode == "LIDAR":
             out = x["left_disp"]
         elif self.mode == "LIDAR-RGB":
-
             out = 0.95*x["left_rgb"] + 0.05*torch.cat((x["left_disp"], x["left_disp"], x["left_disp"]), 1)
         else:
             out = x["left_rgb"]
